f4d57d131d7f292984e29d919adad98ef687424c
anpapag1 <anpapag1@cs.ihu.gr>
2025-04-07 18:39:11 +0300

    PID no web working trying to figure out the web interface

--- NiaFollower/NiaFollower.ino
+++ NiaFollower/NiaFollower.ino
@@ -18,13 +18,13 @@ uint16_t sensorValues[SensorCount];
 #define STBY 4   // Standby pin
 
 // PID parameters
-double Kp = 0.02;  // Start with conservative values
-double Ki = 0.0;
-double Kd = 0.00;
+double Kp = 0.008;  // Start with conservative values
+double Ki = 0;
+double Kd = 0.015;
 
-float Pvalue;
-float Ivalue;
-float Dvalue;
+// Serial command buffer
+String inputString = "";
+bool stringComplete = false;
 
 uint16_t position;
 int P, D, I, previousError, PIDvalue, error;
@@ -49,38 +49,57 @@ void setup() {
   qtr.setTypeAnalog();
   qtr.setSensorPins((const uint8_t[]){ 26, 25, 33, 32, 35, 34, 39, 36}, SensorCount);
   qtr.setEmitterPin(27);
-  Serial.begin(9600);
+  Serial.begin(500000);
+  Serial.println("PID Line Follower Control");
+  processCommand("h "); // Show help on startup
   
   analogReadResolution(12);  // Set ADC resolution to 12 bits
   analogSetAttenuation(ADC_11db);  // Set ADC attenuation for higher voltage range
 
   // calibrate sensors min max
-  calibrateSensors();
+  // calibrateSensors();
 }
 
 void calibrateSensors() {
-  Serial.println("Calibration mode activated");
-  digitalWrite(STBY, LOW); // Disable motors 
-  digitalWrite(LED_BUILTIN, HIGH);
-  for (uint16_t i = 0; i < 200; i++) qtr.calibrate();
-  digitalWrite(LED_BUILTIN, LOW);
-  Serial.println("Calibration complete");
+  // Serial.println("Calibration mode activated");
+  // digitalWrite(STBY, LOW); // Disable motors 
+  // digitalWrite(LED_BUILTIN, HIGH);
+  // for (uint16_t i = 0; i < 200; i++) qtr.calibrate();
+  // digitalWrite(LED_BUILTIN, LOW);
+  // Serial.println("Calibration complete");
   
-  // print calibration values
-  for (uint8_t i = 0; i < SensorCount; i++) {
-    Serial.print(qtr.calibrationOn.minimum[i]);
-    Serial.print(' ');
-  }
-  Serial.println();
-  for (uint8_t i = 0; i < SensorCount; i++) {
-    Serial.print(qtr.calibrationOn.maximum[i]);
-    Serial.print(' ');
-  }
-  Serial.println();
-  delay(1000);
+  // // print calibration values
+  // for (uint8_t i = 0; i < SensorCount; i++) {
+  //   Serial.print(qtr.calibrationOn.minimum[i]);
+  //   Serial.print(' ');
+  // }
+  // Serial.println();
+  // for (uint8_t i = 0; i < SensorCount; i++) {
+  //   Serial.print(qtr.calibrationOn.maximum[i]);
+  //   Serial.print(' ');
+  // }
+  // Serial.println();
+  // delay(1000);
 }
 
 void loop() {
+  // Process serial commands
+  if (stringComplete) {
+    processCommand(inputString);
+    inputString = "";
+    stringComplete = false;
+  }
+
+  // Read serial data
+  while (Serial.available()) {
+    char inChar = (char)Serial.read();
+    if (inChar == '\n') {
+      stringComplete = true;
+    } else {
+      inputString += inChar;
+    }
+  }
+
   if (digitalRead(CAL_BTN) != HIGH || Calibration) {
     Calibration = false;
     calibrateSensors();
@@ -92,11 +111,48 @@ void loop() {
     while(digitalRead(STRT_BTN) != HIGH) {}
   }
 
-  position = readLine(position, 4000);
-  Serial.println(position);
+  position = readLine(position, 4090);
 
   error = 3500 - position; 
   PID_Linefollow(error);
+}
+
+void processCommand(String command) {
+  if (command.length() < 1) return;
+  
+  char cmd = command.charAt(0);
+  float value = 0;
+  
+  if (command.length() > 1) {
+    value = command.substring(1).toFloat();
+  }
+  
+  switch (cmd) {
+    case 'p':
+      Kp = value;
+      Serial.println("Kp set to: "+String(Kp,4));
+      break;
+    case 'i':
+      Ki = value;
+      Serial.println("Ki set to: "+String(Ki,4));
+      break;
+    case 'd':
+      Kd = value;
+      Serial.println("Kd set to: "+String(Kd,4));
+      break;
+    case 's':
+      baseSpeed = (int)value;
+      Serial.println("Base speed set to: "+String(baseSpeed));
+      break;
+    case 'h':
+      Serial.println("Commands:");
+      Serial.println("p = "+String(Kp,4)+" // Set Kp");
+      Serial.println("i = "+String(Ki,4)+" // Set Ki");
+      Serial.println("d = "+String(Kd,4)+" // Set Kd");
+      Serial.println("s = "+String(baseSpeed)+" // Set base speed");
+      Serial.println("h - Show this help");
+      break;
+  }
 }
 
 void PID_Linefollow(int error) {
@@ -104,18 +160,11 @@ void PID_Linefollow(int error) {
   I = I + error;
   D = error - previousError;
   
-  Pvalue = Kp * P;
-  Ivalue = Ki * I;
-  Dvalue = Kd * D; 
-
-  float PIDvalue = Pvalue + Ivalue + Dvalue;
+  float PIDvalue = Kp * P + Ki * I + Kd * D;
   previousError = error;
-
-  lsp = baseSpeed + PIDvalue;
-  rsp = baseSpeed - PIDvalue;
   
-  lsp = constrain(lsp, -255, 255);
-  rsp = constrain(rsp, -255, 255);
+  lsp = constrain(baseSpeed + PIDvalue, -255, 255);
+  rsp = constrain(baseSpeed - PIDvalue, -255, 255);
 
   motor_drive(lsp, rsp);
 }
@@ -133,45 +182,60 @@ int readLine(int lastposition, int blackLineValue){
   }
 
   // calculate the average position
-  if (count != 0) {
+  if (count == 8) {
+    robotEnabled = false;
+  } else if (count != 0) {
     position = position / count; // average the position
   } else {
     if (lastposition > 3500) {
-      position = lastposition; // no sensors detected, set position to last known
+      position = 7500; // no sensors detected, set position to last known
     } else {
-      position = 0; // no sensors detected, set position to 0
+      position = -500; // no sensors detected, set position to 0
     }
   }
 
-  for (uint8_t i = 0; i < SensorCount; i++){
-    if (sensorValues[i] > blackLineValue) {
-      Serial.print("#");
-    } else {
-      Serial.print("_");
-    }
-  }
-  Serial.print("\t position: " );
+  // for (uint8_t i = 0; i < SensorCount; i++){
+  //   if (sensorValues[i] > blackLineValue) {
+  //     Serial.print("#");
+  //   } else {
+  //     Serial.print("_");
+  //   }
+  // }
+  // Serial.print("\t position: " );
+  // Serial.println(position);
   return position;
 }
 
 void motor_drive(int left, int right) {
   digitalWrite(STBY, robotEnabled ? HIGH : LOW);
 
+  // todo stop motor if psoition 7000
   // Left motor
-  if (left > 0) {
-    analogWrite(AIN1, left);
-    analogWrite(AIN2, 0);
-  } else {
-    analogWrite(AIN1, 0);
-    analogWrite(AIN2, abs(left));
-  }
+  // if (position != 7000) {
+    if (left > 0) {
+      analogWrite(AIN1, left);
+      analogWrite(AIN2, 0);
+    } else {
+      analogWrite(AIN1, 0);
+      analogWrite(AIN2, abs(left));
+    }
+  // } else {
+  //   analogWrite(AIN1, 0);
+  //   analogWrite(AIN2, 30);
+  // }
   
+  // todo stop motor if psoition 0
   // Right motor
-  if (right > 0) {
-    analogWrite(BIN1, right);
-    analogWrite(BIN2, 0);
-  } else {
-    analogWrite(BIN1, 0);
-    analogWrite(BIN2, abs(right));
-  }
+  // if (position != 0) {
+    if (right > 0) {
+      analogWrite(BIN1, right);
+      analogWrite(BIN2, 0);
+    } else {
+      analogWrite(BIN1, 0);
+      analogWrite(BIN2, abs(right));
+    }
+  // } else {
+  //   analogWrite(BIN1, 0);
+  //   analogWrite(BIN2, 30);
+  // }
 }
